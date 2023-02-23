2ac5beca66f65d35516b06233326b3d7ba604f42
ColeCollector <colen827@deltalearns.ca>
2023-02-23 11:24:58 -0800

    done

---
+++ assignment2.py
@@ -0,0 +1,6 @@
+#! python3
+
+# SD Computing Studies Assignment
+
+fname = str("Mr")
+lname = str("Yang")
---
+++ assignment3.py
@@ -0,0 +1,6 @@
+#! python3
+
+# SD Computing Studies Assignment
+
+value1 = 10
+value2 = str("10")
